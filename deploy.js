bf14404315a98b1328452c04a03308dd8f81b0d8
Michael Maier <maier1michael@gmail.com>
2018-02-07 16:04:42 +0100

    Updates

--- deploy.js
+++ deploy.js
@@ -1,14 +1,8 @@
 var ghpages = require('gh-pages');
 
-ghpages.publish(process.cwd());
-// , {
-//     // user: {
-//     //     name: "Michael Maier",
-//     //     email: "maier1michael@gmail.com"
-//     // },
-//     logger: function (message) {
-//         console.log(message)
-//     }
-// }, function (err) {
-//     console.error(err)
-// })
+ghpages.publish(process.cwd(), {
+    user: {
+        name: "Michael Maier",
+        email: "maier1michael@gmail.com"
+    }
+});
